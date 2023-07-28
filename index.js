8c1d525cd9719a9869d33221d87812b33a4211e4
Karan <karanchandekar431@gmail.com>
2023-07-28 22:48:15 +0530

    feat: api and routing

--- index.js
+++ index.js
@@ -3,10 +3,29 @@ const path = require("path");
 
 const app = express();
 
+app.use(express.static(path.join(path.resolve(), "public")));
+app.use(express.urlencoded({ extended: true }));
+
+const users = [];
+
 app.get("/", (req, res) => {
   res.render("index.ejs", { name: "Karan" });
 });
 
-app.listen(3000, () => {
+app.get("/success", (req, res) => {
+  res.render("success.ejs");
+});
+
+app.post("/contact", (req, res) => {
+  users.push({ name: req.body.name, email: req.body.email });
+  res.redirect("/success");
+  console.log(users);
+});
+
+app.get("/users", (req, res) => {
+  res.json({ users });
+});
+
+app.listen(5000, () => {
   console.log("Example app listening on port 3000!");
 });
