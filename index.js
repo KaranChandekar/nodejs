a93f2532f8a4492e393dea349fad2f3a9f6a4827
Karan <karanchandekar431@gmail.com>
2023-07-29 17:42:33 +0530

    feat: database

--- index.js
+++ index.js
@@ -1,25 +1,46 @@
 const express = require("express");
 const path = require("path");
+const mongoose = require("mongoose");
 
 const app = express();
 
 app.use(express.static(path.join(path.resolve(), "public")));
 app.use(express.urlencoded({ extended: true }));
 
-const users = [];
+mongoose
+  .connect("mongodb://localhost:27017", {
+    dbName: "test",
+  })
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((err) => console.error(err));
+
+const studentSchema = new mongoose.Schema({
+  name: String,
+  email: String,
+});
+
+const Student = mongoose.model("students", studentSchema);
 
 app.get("/", (req, res) => {
   res.render("index.ejs", { name: "Karan" });
+});
+
+app.get("/add", (req, res) => {
+  Student.create({ name: "Unknown", email: "unknown@gmail.com" }).then(() => {
+    res.send("Nice");
+  });
 });
 
 app.get("/success", (req, res) => {
   res.render("success.ejs");
 });
 
-app.post("/contact", (req, res) => {
-  users.push({ name: req.body.name, email: req.body.email });
+app.post("/contact", async (req, res) => {
+  const { name, email } = req.body;
+  await Student.create({ name, email });
   res.redirect("/success");
-  console.log(users);
 });
 
 app.get("/users", (req, res) => {
@@ -27,5 +48,5 @@ app.get("/users", (req, res) => {
 });
 
 app.listen(5000, () => {
-  console.log("Example app listening on port 3000!");
+  console.log("Example app listening on port 5000!");
 });
