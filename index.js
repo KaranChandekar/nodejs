7b2a1fbe838b10e8be4960b3e6dd4399f7c59ca1
Karan <karanchandekar431@gmail.com>
2023-07-30 13:31:42 +0530

    feat: authentication

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 const express = require("express");
 const path = require("path");
 const mongoose = require("mongoose");
+const cookieParser = require("cookie-parser");
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcrypt");
 
 const app = express();
 
 app.use(express.static(path.join(path.resolve(), "public")));
 app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
 
 mongoose
   .connect("mongodb://localhost:27017", {
@@ -16,37 +20,87 @@ mongoose
   })
   .catch((err) => console.error(err));
 
-const studentSchema = new mongoose.Schema({
+const userSchema = new mongoose.Schema({
   name: String,
   email: String,
+  password: String,
 });
 
-const Student = mongoose.model("students", studentSchema);
+const User = mongoose.model("users", userSchema);
 
-app.get("/", (req, res) => {
-  res.render("index.ejs", { name: "Karan" });
+const isAuthenticated = async (req, res, next) => {
+  const { token } = req.cookies;
+  if (token) {
+    const decodedToken = jwt.verify(token, "lskdjflsdjfoissjsjh");
+    req.user = await User.findById(decodedToken._id);
+    next();
+  } else {
+    res.redirect("/login");
+  }
+};
+
+app.get("/", isAuthenticated, (req, res) => {
+  res.render("logout.ejs", { user: req.user.name });
 });
 
-app.get("/add", (req, res) => {
-  Student.create({ name: "Unknown", email: "unknown@gmail.com" }).then(() => {
-    res.send("Nice");
+app.get("/register", (req, res) => {
+  res.render("register.ejs");
+});
+
+app.get("/login", (req, res) => {
+  res.render("login.ejs");
+});
+
+app.post("/login", async (req, res) => {
+  const { email, password } = req.body;
+
+  const user = await User.findOne({ email });
+  if (!user) return res.redirect("/register");
+
+  const isMatch = await bcrypt.compare(password, user.password);
+  if (!isMatch)
+    return res.render("login.ejs", { message: "Invalid password", email });
+
+  const token = jwt.sign({ _id: user._id }, "lskdjflsdjfoissjsjh");
+
+  res.cookie("token", token, {
+    httpOnly: true,
+    expires: new Date(Date.now() + 60 * 1000),
   });
+  res.redirect("/");
 });
 
-app.get("/success", (req, res) => {
-  res.render("success.ejs");
+app.post("/register", async (req, res) => {
+  const { name, email, password } = req.body;
+
+  const user = await User.findOne({ email });
+  if (user) return res.redirect("/login");
+
+  const hashedPassword = await bcrypt.hash(password, 10);
+
+  const newUser = await User.create({
+    name,
+    email,
+    password: hashedPassword,
+  });
+
+  const token = jwt.sign({ _id: newUser._id }, "lskdjflsdjfoissjsjh");
+
+  res.cookie("token", token, {
+    httpOnly: true,
+    expires: new Date(Date.now() + 60 * 1000),
+  });
+  res.redirect("/");
 });
 
-app.post("/contact", async (req, res) => {
-  const { name, email } = req.body;
-  await Student.create({ name, email });
-  res.redirect("/success");
-});
-
-app.get("/users", (req, res) => {
-  res.json({ users });
+app.get("/logout", (req, res) => {
+  res.cookie("token", "null", {
+    httpOnly: true,
+    expires: new Date(Date.now()),
+  });
+  res.redirect("/");
 });
 
 app.listen(5000, () => {
-  console.log("Example app listening on port 5000!");
+  console.log("Node app listening on port 5000!");
 });
